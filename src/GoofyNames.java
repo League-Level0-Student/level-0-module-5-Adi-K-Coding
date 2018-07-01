0f73f0140db12d3220762c563f28265062567010
Adi-K-Coding <league@WTS13.attlocal.net>
2018-07-01 16:00:44 -0700

    goofs & guessing pie

--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -1,4 +1,6 @@
+
 /*
+
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
  */
@@ -10,29 +12,33 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-String name = JOptionPane.showInputDialog("What is yor name?");
+		String name = JOptionPane.showInputDialog("What is yor name?");
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
-System.out.println(name.toUpperCase());
+		// Run your program to see that this works.
+		System.out.println(name.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
+		// HINT: Use .length() to determine the number of characters in the String.
 
-for (int i = 0; i <name.length(); i++) {
-	System.out.println(name.charAt(i));
-}
-			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
-			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
-
-			
-			// 6. ADD this String  (containing 1 char) to the goofyName String
-
+		for (int i = 0; i < name.length(); i++) {
+			System.out.println(name.charAt(i));
 		
+		// 4. Create a String variable to store the next character of the name
+		// using .substring(start, end)
+		// HINT: replace 'start' and 'end' to get String with character at i
+String nextvariable = name.substring(i, i+1);
+;		// 5. Use MODULO to set this variable to upper case for EVEN characters
+		// and lower case for ODD characters.
+if(i%2==0) {
+nextvariable = nextvariable.toUpperCase();
+}else {
+nextvariable = nextvariable.toLowerCase();
+}
+		// 6. ADD this String (containing 1 char) to the goofyName String
+		goofyName+=nextvariable;
+		
+		
+		}
 		// 7. Use pop-up to show user their Goofy name
-
+JOptionPane.showMessageDialog(null, goofyName);
 	}
 }
-
